54037dce697db830ded794914f7d52bdc7679c56
katherinestout <kjs14@hood.edu>
2018-09-28 15:27:54 -0400

    prop types

---
+++ src/components/Props.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+//import PropTypes from 'prop-types';
+
+
+class Props extends Component {
+  render() {
+      const Headline = () => {
+          return <h1 className="title">Welcome to React</h1>
+      }
+      const Greeting =(props) => {
+        const {name, age} = props;
+        return <p>
+            You will love it {name} ({age})! 
+        </p>
+      }
+    return (
+      <div>
+       <Headline/>
+       <Greeting name = "Kate" age = {23} />
+      </div>
+    );
+  }
+}
+/*
+Greeting.propTypes ={
+    name: PropTypes.string,
+    age: PropTypes.number
+}
+*/
+export default Props;
