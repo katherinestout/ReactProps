bc390b4466ba543c752a5817f087dd6f343ed940
katherinestout <kjs14@hood.edu>
2018-09-30 20:26:14 -0400

    more

---
+++ src/components/MoreProps.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import PropTypes from 'prop-types';
+
+
+class MoreProps extends Component {
+  render() {
+  
+    return (
+      <div>
+       
+      </div>
+    );
+  }
+}
+
+
+export default MoreProps;
